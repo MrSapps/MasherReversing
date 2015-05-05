4819e627b8de91730e9bb11960e01cb7d521ada4
Paul <Paul@paul.com>
2015-05-05 19:58:05 +0100

    a little more tidying up

--- MasherReversing/Hooks.cpp
+++ MasherReversing/Hooks.cpp
@@ -57,8 +57,8 @@ typedef decltype(&ddv__func5_block_decoder_q) ddv__func5_block_decoder_q_type;
 ddv__func5_block_decoder_q_type real_ddv__func5_block_decoder_q = (ddv__func5_block_decoder_q_type)0x00409FE0;
 JmpHookedFunction<ddv__func5_block_decoder_q_type>* ddv_func6_decodes_block_q_hook;
 
-
-/*int __declspec (naked)*/ void  calling_conv_hack(int param)
+// Wrappers for hand made calling conventions
+void do_write_block_bit1_no_mmx(int param)
 {
     __asm
     {
@@ -69,8 +69,7 @@ JmpHookedFunction<ddv__func5_block_decoder_q_type>* ddv_func6_decodes_block_q_ho
     }
 }
 
-//B2, D6 07
-/*int __declspec (naked)*/ void  calling_conv_hack2(int macroBlockBuffer, int* decodedBitStream)
+void do_blit_output_no_mmx(int macroBlockBuffer, int* decodedBitStream)
 {
     __asm
     {
@@ -85,13 +84,12 @@ char __fastcall ddv__func5_block_decoder_q(void* hack, ddv_class *thisPtr, unsig
 {
     OutputDebugString("ddv__func5_block_decoder_q\n");
 
-    // Take the non MMX path
+    // Take the non MMX path - this function replaces the code that was gurded by this, probably don't matter what its set to now :)
     *p_gCpuSupportsMMX = false;
 
     int(__cdecl *decodeMacroBlockfPtr)(int, int *, int, DWORD, int, int *); // edi@2
 
     int decode6_mmx_ret = 0; // ebx@4
-    int dataSizeDWords = 0; // ebp@4
     int buffer = 0; // esi@4
     int dataSizeBytes = 0; // ebp@7
     int decode1_ret = 0; // ebx@14
@@ -133,8 +131,6 @@ char __fastcall ddv__func5_block_decoder_q(void* hack, ddv_class *thisPtr, unsig
     after_block_decode_no_effect_q_ptr(v9); // actually does have an effect
     decode6_mmx_ret = (int)thisPtr->mDecodedBitStream;
 
-    dataSizeDWords = thisPtr->mBlockDataSize_q; // 64 - because 64 coefficients?
-
     // Sanity check
     if (thisPtr->nNumMacroblocksX <= 0 || thisPtr->nNumMacroblocksY <= 0)
     {
@@ -149,40 +145,40 @@ char __fastcall ddv__func5_block_decoder_q(void* hack, ddv_class *thisPtr, unsig
         for (unsigned int yBlock = 0; yBlock < thisPtr->nNumMacroblocksY; yBlock++)
         {
             // B1
-            decode1_ret = decodeMacroBlockfPtr(decode6_mmx_ret, p_gMacroBlock1Buffer, buffer, 0 /*14h*/, 0, 0);
-            calling_conv_hack2(buffer, p_gMacroBlock1Buffer); // << didn't change gMacroBlock1Buffer!
+            decode1_ret = decodeMacroBlockfPtr(decode6_mmx_ret, p_gMacroBlock1Buffer, buffer, 0, 0, 0);
+            do_blit_output_no_mmx(buffer, p_gMacroBlock1Buffer);
 
-            dataSizeBytes = 4 * dataSizeDWords;
+            dataSizeBytes = 4 * thisPtr->mBlockDataSize_q;
             block2Ptr = dataSizeBytes + buffer;
 
             // B2
-            decode2_ret = decodeMacroBlockfPtr(decode1_ret, p_gMacroBlock2Buffer, block2Ptr, 0, buffer, p_gMacroBlock1Buffer); // last 2 args are unused?
-            calling_conv_hack2(block2Ptr, p_gMacroBlock2Buffer);
+            decode2_ret = decodeMacroBlockfPtr(decode1_ret, p_gMacroBlock2Buffer, block2Ptr, 0, buffer, p_gMacroBlock1Buffer);
+            do_blit_output_no_mmx(block2Ptr, p_gMacroBlock2Buffer);
             block3Ptr = dataSizeBytes + block2Ptr;
 
             // B3
             decode3_ret = decodeMacroBlockfPtr(decode2_ret, p_gMacroBlock3Buffer, block3Ptr, 1, block2Ptr, p_gMacroBlock2Buffer);
-            calling_conv_hack2(block3Ptr, p_gMacroBlock3Buffer);
+            do_blit_output_no_mmx(block3Ptr, p_gMacroBlock3Buffer);
             block4Ptr = dataSizeBytes + block3Ptr;
 
             // B4
             decode4_ret = decodeMacroBlockfPtr(decode3_ret, p_gMacroBlock4Buffer, block4Ptr, 1, block3Ptr, p_gMacroBlock3Buffer);
-            calling_conv_hack2(block4Ptr, p_gMacroBlock4Buffer);
+            do_blit_output_no_mmx(block4Ptr, p_gMacroBlock4Buffer);
             block5Ptr = dataSizeBytes + block4Ptr;
 
             // B5
             decode5_ret = decodeMacroBlockfPtr(decode4_ret, p_gMacroBlock5Buffer, block5Ptr, 1, block4Ptr, p_gMacroBlock4Buffer);
-            calling_conv_hack2(block5Ptr, p_gMacroBlock5Buffer);
+            do_blit_output_no_mmx(block5Ptr, p_gMacroBlock5Buffer);
             block6Ptr = dataSizeBytes + block5Ptr;
 
             // B6
             decode6_mmx_ret = decodeMacroBlockfPtr(decode5_ret, p_gMacroBlock6Buffer, block6Ptr, 1, block5Ptr, p_gMacroBlock5Buffer);
-            calling_conv_hack2(block6Ptr, p_gMacroBlock6Buffer);
+            do_blit_output_no_mmx(block6Ptr, p_gMacroBlock6Buffer);
             buffer = dataSizeBytes + block6Ptr;
 
             if (dword_62EFE0 & 1) // Hit on 2x2 dithering
             {
-                calling_conv_hack((int)pScreenBufferCurrentPos);
+                do_write_block_bit1_no_mmx((int)pScreenBufferCurrentPos);
                 //  write_block_bit1_no_mmx_ptr(dataSizeBytes);// sets width too small and images look weird
             }
             else if (dword_62EFE0 & 4)
@@ -212,8 +208,6 @@ char __fastcall ddv__func5_block_decoder_q(void* hack, ddv_class *thisPtr, unsig
             {
                 pScreenBufferCurrentPos = (unsigned __int8 *)(16 * dword_62EFD8 + v36);
             }
-
-            dataSizeDWords = dataSizeDWords;
         }
         pScreenBuffer += dword_62EFD4;              // not sure what this is, height maybe? No scaling = 32, 2x1 = 64, 2x2=64
     }
