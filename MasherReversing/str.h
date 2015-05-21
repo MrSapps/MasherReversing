b69c1821f621511dce151bee7177deb8e9bcccbd
paulm <github@paulsapps.com>
2015-05-21 14:05:53 +0100

    add jpsxdec str doc for ref

--- MasherReversing/str.h
+++ MasherReversing/str.h
@@ -13,34 +13,35 @@ struct SectorHeader
 
 struct PsxStrVideoHeader
 {
+    // The 4 "unknown" / 0x80010160 in psx data is replaced by "AKIK" in PC data
     /*
     0000  char   {4}     "AKIK"
+
+    // This is standard STR format data here
     0004  uint16 {2}     Sector number within frame (zero-based)
     0006  uint16 {2}     Number of sectors in frame
     0008  uint32 {4}     Frame number within file (one-based)
     000C  uint32 {4}     Frame data length
     0010  uint16 {2}     Video Width
     0012  uint16 {2}     Video Height
+
     0014  uint16 {2}     Number of MDEC codes divided by two, and rounded up to a multiple of 32
     0016  uint16 {2}     Always 0x3800
     0018  uint16 {2}     Quantization level of the video frame
     001A  uint16 {2}     Version of the video frame (always 2)
     001C  uint32 {4}     Always 0x00000000
     */
+
+    //demultiplexing is joining all of the frame data into one buffer without the headers
+
 };
 
 struct MasherVideoHeaderWrapper
 {
-    DWORD mMagic; // AKAIK
+    DWORD mMagic; // AKIK
     PsxStrVideoHeader mStrHeader;
 };
 
-/*
-0000  uint16 {2}     Number of MDEC codes divided by two, and rounded up to a multiple of 32
-0002  uint16 {2}     Always 0x3800
-0004  uint16 {2}     Quantization level of the video frame
-0006  uint16 {2}     Version of the video frame (always 2)
-*/
 
 // The video is encoded using the same encoding as used by the PSX STR format.
 
